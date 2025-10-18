0c72ed4f2be9abcd318d67c32050642abfab079e
IMRANE EL AGGAL <I.ELAGGAL@wafaimmo.local>
2025-10-18 15:41:46 +0200

    Add helmet.hidePoweredBy middleware for security

--- myApp.js
+++ myApp.js
@@ -1,24 +1,22 @@
-var express = require('express');
-var app = express();
-
-/******************************************************
- * PLEASE DO NOT EDIT ABOVE THIS LINE
- *******************************************************/
+const express = require('express');
+const app = express();
 
 var helmet = require('helmet');
 
-// Use helmet to set default security headers
-app.use(helmet());
-
-// Remove X-Powered-By header to hide Express info
 app.use(helmet.hidePoweredBy());
 
-/******************************************************
- * PLEASE DO NOT EDIT BELOW THIS LINE
- *******************************************************/
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`Your app is listening on port ${PORT}`);
-});
+
+
 
 module.exports = app;
+const api = require('./server.js');
+app.use(express.static('public'));
+app.disable('strict-transport-security');
+app.use('/_api', api);
+app.get("/", function (request, response) {
+  response.sendFile(__dirname + '/views/index.html');
+});
+let port = process.env.PORT || 5000;
+app.listen(port, '0.0.0.0', () => {
+  console.log(`Your app is listening on port ${port}`);
+});
